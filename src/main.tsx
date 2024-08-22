6c27c7acca10ae52a6bb1e1cbef7ab24d56d08ac
Iago Viotti <iago.viotti@gmail.com>
2024-08-21 21:27:27 -0300

    no css timed loading screen implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
 import { ChatProvider } from './hooks/useChat'
 import { AudioProvider } from './hooks/useRubberDuckSound'
+import SuspenseWrapper from './Components/Suspense'
 
 createRoot(document.getElementById('root') as HTMLElement)?.render(
   <AudioProvider>
     <ChatProvider>
-      <App />
+        <SuspenseWrapper />
     </ChatProvider>
   </AudioProvider>
 )
